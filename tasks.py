ec629ea4a6a684f37b8b0396dc7d94a767e1b181
DVDH-000 <davy.vanderhorst@springbokagency.com>
2023-10-10 12:03:44 +0200

    Updated references

--- tasks.py
+++ tasks.py
@@ -1,7 +1,7 @@
 from invoke import task
 
 _TEST_FOLDER = "tests"
-_SOURCE_FOLDERS = " ".join(["bq_schema", _TEST_FOLDER])
+_SOURCE_FOLDERS = " ".join(["bq_schema_policy_tags", _TEST_FOLDER])
 
 
 @task
@@ -23,7 +23,7 @@ def check_code_format(context):
 @task
 def test(context):
     context.run(
-        f"pytest {_TEST_FOLDER} --doctest-modules --junitxml=junit/test-results.xml --cov=bq_schema --cov-report=xml --cov-report=html"
+        f"pytest {_TEST_FOLDER} --doctest-modules --junitxml=junit/test-results.xml --cov=bq_schema_policy_tags --cov-report=xml --cov-report=html"
     )
 
 
